4565b858c0d1f5dafe4d6c13088d09819e2c570f
lauthekiller <39975051+lauthekiller@users.noreply.github.com>
2026-03-03 17:19:10 -0500

    suputamadre

---
+++ VR/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("Paneles")]
+    public GameObject panelMenu;
+    public GameObject panelCreditos;
+    public GameObject panelOpciones;
+
+    [Header("Audio")]
+    public AudioSource musicaFondo;
+    public Slider sliderVolumen;
+
+    void Start()
+    {
+        panelMenu.SetActive(true);
+        panelCreditos.SetActive(false);
+        panelOpciones.SetActive(false);
+
+        if (sliderVolumen != null)
+        {
+            sliderVolumen.value = musicaFondo.volume;
+            sliderVolumen.onValueChanged.AddListener(CambiarVolumen);
+        }
+    }
+
+    // ▶️ INICIAR JUEGO
+    public void IniciarJuego()
+    {
+        SceneManager.LoadScene("NombreDeTuEscenaDelJuego");
+    }
+
+    // 🎬 MOSTRAR CREDITOS
+    public void MostrarCreditos()
+    {
+        panelMenu.SetActive(false);
+        panelCreditos.SetActive(true);
+    }
+
+    // ⚙️ MOSTRAR OPCIONES
+    public void MostrarOpciones()
+    {
+        panelMenu.SetActive(false);
+        panelOpciones.SetActive(true);
+    }
+
+    // 🔙 VOLVER AL MENU
+    public void VolverMenu()
+    {
+        panelMenu.SetActive(true);
+        panelCreditos.SetActive(false);
+        panelOpciones.SetActive(false);
+    }
+
+    // 🔊 CAMBIAR VOLUMEN
+    public void CambiarVolumen(float valor)
+    {
+        musicaFondo.volume = valor;
+    }
+
+    // ❌ SALIR
+    public void Salir()
+    {
+        Application.Quit();
+        Debug.Log("Juego cerrado");
+    }
+}
